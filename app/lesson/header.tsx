145e82047aa3fb7091b87098a150cdc520bcee2b
yash-mewada <yashmewada27@gmail.com>
2024-03-28 22:49:37 +0530

    create exit modal

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,4 +1,5 @@
 import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
 import { InfinityIcon, X } from "lucide-react";
 import Image from "next/image";
 
@@ -13,10 +14,12 @@ export const Header = ({
   percentage,
   hasActiveSubscription,
 }: Props) => {
+  const { open } = useExitModal();
+
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <X
-        onClick={() => {}}
+        onClick={open}
         className="text-slate-500 hover:opacity-75 transition cursor-pointer"
       />
       <Progress value={percentage} />
